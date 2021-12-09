930768d4cb7ecf77cc27cd76c2985d4b9ebfe01a
savitapal8 <78473714+savitapal8@users.noreply.github.com>
2021-12-09 15:09:33 +0530

    Update lb.tf

--- lb.tf
+++ lb.tf
@@ -54,7 +54,8 @@ resource "google_compute_forwarding_rule" "google_compute_forwarding_rule" {
   region                = "europe-west1"
   depends_on            = [google_compute_subnetwork.proxy_subnet]
   ip_protocol           = "TCP"
-  load_balancing_scheme = "INTERNAL_MANAGED"
+  #load_balancing_scheme = "INTERNAL_MANAGED"
+  load_balancing_scheme = "EXTERNAL"
   port_range            = "443"
   all_ports             = true
   target                = google_compute_region_target_http_proxy.default.id
